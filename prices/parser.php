db692e4b88b7a2691c49ebd63c3df31e2b3221b8
Roman Alyakritskiy <rawork@yandex.ru>
2017-07-02 09:03:58 +0300

    parser init

--- prices/parser.php
+++ prices/parser.php
@@ -1,17 +1,49 @@
+#!/usr/bin/env php
 <?php
 
-require_once('vendor/autoload.php');
+$loader = require __DIR__.'/vendor/autoload.php';
+
+define('CURRENT_JSON', 'app/current.json');
 
 if (php_sapi_name() != 'cli') {
     die('Commandline mode only accepted'."\n");
 }
 
-
-
-if (file_exists(GOOGLE_CATEGORIES_XSLX_PATH)) {
-	$objPHPExcel = \PHPExcel_IOFactory::createReader('Excel2007');
-	$objPHPExcel = $objPHPExcel->load(GOOGLE_CATEGORIES_XSLX_PATH);
-	$objPHPExcel->setActiveSheetIndex(0);
-	$i = $objPHPExcel->getActiveSheet()->getHighestRow()+1;
+$current = false;
+if (file_exists(CURRENT_JSON)) {
+	$current = json_decode(file_get_contents(CURRENT_JSON),true);
 }
+
+if ($current && isset($current['file']) && isset($current['position'])) {
+	$file = $current['file'];
+	$position = $current['position'];
+
+} else {
+	$files = glob('upload/*.xlsx');
+
+	if (!$files) {
+		echo "No price .xlsx files\n";
+		exit;
+	}
+
+	usort($files, function($a, $b){
+		return filemtime($a) - filemtime($b);
+	});
+
+	$file = __DIR__ . '/' . array_shift($files);
+
+	$position = 2;
+}
+
+//	$objPHPExcel = \PHPExcel_IOFactory::createReader('Excel2007');
+//	$objPHPExcel = $objPHPExcel->load($file);
+//	$objPHPExcel->setActiveSheetIndex(0);
+
+
+echo "{$file} - {$position}\n";
+
+
+
+
+
 
